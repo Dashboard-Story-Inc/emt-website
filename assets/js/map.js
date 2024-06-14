bdca7ebb3d0d9753533342d356a01e1758b47e44
Gam Petralba <g.petralba@arcanys.com>
2024-06-14 16:33:56 +0800

    feat(map): center at Bakersfield

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,8 +4,8 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 35.357871, lng: -119.043732 },
+    zoom: 11
   });
 }
 
